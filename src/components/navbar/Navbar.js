dd51742be746ca2c4e7899a8552f204818ae68b3
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-07-14 22:04:02 +0530

    add create account

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../../assets/logo.svg";
 import navDropDown from "../../assets/navDropDown.svg";
 import searchIcon from "../../assets/searchNavbar.svg";
+import hero_svg from '../../assets/hero-form.svg'
 
 function Navbar() {
   return (
@@ -52,21 +53,42 @@ function Navbar() {
         <div class="modal-dialog modal-lg modal-dialog-centered ">
           <div class="modal-content">
             <div className='modal-header'>
-             
-                <p>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</p>
-  
-            </div>
-             <div className=" p-4 contents d-flex align-items-center justify-content-between">
-                 <div className='form'>
-                     <h1 className="form_heading">Create Account</h1>
-                 </div>
-                 <div className="svg">
-                     <p className="svg_heading">Already have an account? <span>Sign In</span></p>
-                 </div>
-             </div>
-            </div>
+
+              <p>Let's learn, share & inspire each other with our passion for computer engineering. Sign up now 🤘🏼</p>
 
             </div>
+
+            <div className=" p-4 contents d-flex justify-content-between ">
+              <div className=' w-50 form d-flex flex-column   '>
+                <h1 className="form_heading mb-2">Create Account</h1>
+                <form class="row ">
+                  <div class="col-6">
+                    <input type="email" class="form-control form-control-lg" id="inputEmail4" />
+                  </div>
+                  <div class="col-6">
+                    <input type="password" class="form-control form-control-lg" id="inputPassword4" />
+                  </div>
+                  <div class="col-12">
+                    <input type="text" class="form-control form-control-lg" id="inputAddress" placeholder="Email" />
+                  </div>
+                  <div class="col-12">
+                    <input type="text" class="form-control form-control-lg" id="inputAddress2" placeholder="Password" />
+                  </div>
+                  <div class="col-12">
+                    <input type="text" class="form-control form-control-lg" id="inputAddress2" placeholder="Confirm Password" />
+                  </div>
+                </form>
+
+              </div>
+              <div className=" w-50 svg d-flex flex-column justify-content-center">
+                <p className="svg_heading mb-2">Already have an account? <span>Sign In</span></p>
+                <img src={hero_svg} alt="" />
+                <p>By signing up, you agree to our Terms & conditions, Privacy policy</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
